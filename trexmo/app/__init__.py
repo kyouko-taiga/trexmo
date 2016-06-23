d94f7e90f6071a0412e9add6b627d84eaef925b9
Dimitri Racordon <dimitri.racordon@gmail.com>
2016-06-23 13:02:44 +0200

    implement a redis cache for the flask app

--- trexmo/app/__init__.py
+++ trexmo/app/__init__.py
@@ -1,11 +1,27 @@
+from werkzeug.contrib.cache import RedisCache
+
 from trexmo import factory
 from trexmo.core.utils.encoders import TrexmoJsonEncoder
+
+
+def make_cache(config):
+    backend = config.get('CACHE_BACKEND')
+    cache_config = config.get('CACHE_CONFIG', {})
+
+    if backend == 'Redis':
+        return RedisCache(**cache_config)
+
+    # Currently, only RedisCache is supported.
+    raise ValueError("Unsupported cache backend: '%s'" % backend)
 
 
 def create_app(debug=False):
     """Return an instance of a TREXMO web application."""
     app = factory.create_app(__name__, __path__, debug)
-    
+
+    # Create the app cache.
+    app.cache = make_cache(app.config)
+
     # Set the default json encoder
     app.json_encoder = TrexmoJsonEncoder
 
--- trexmo/settings.py
+++ trexmo/settings.py
@@ -6,6 +6,16 @@ basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 # SQLAlchemy
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data', 'trexmo.db')
 
+# Caching
+CACHE_BACKEND = 'Redis'
+CACHE_CONFIG = {
+    'host': 'localhost',
+    'port': 6379,
+    'password': None,
+    'db': 0,
+    'default_timeout': 300
+}
+
 # Authentication
 SECRET_KEY = '108-e08-c20'
 AUTH_TOKEN_DURATION = 86400
