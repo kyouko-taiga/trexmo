9f3e6000787467c30b55f3d57e38b2266976f8c8
Dimitri Racordon <dimitri.racordon@gmail.com>
2016-06-28 10:13:27 +0200

    memoize the parsing of ddtl models

--- trexmo/core/db/models/form.py
+++ trexmo/core/db/models/form.py
@@ -50,11 +50,21 @@ class Form(Dictionarizable):
 
     @classmethod
     def all(cls, directory):
+        # Note that we can't decorate this method with flask.ext.cache.memoize
+        # because it will can loaded outside of an application context.
+        cache_key = (
+            current_app.config['CACHE_KEY_PREFIX'] + cls.__name__ + '.all?' + directory)
+        rv = current_app.cache.get(cache_key)
+        if rv is not None:
+            return rv
+
         rv = []
         for filename in os.listdir(directory):
             if not filename.startswith('.'):
                 with open(os.path.join(directory, filename), 'r') as f:
                     rv.append(cls.load(f))
+
+        current_app.cache.set(cache_key, rv)
         return rv
 
     @classmethod
